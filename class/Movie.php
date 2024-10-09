ba40a51a5c8782a067fc425fd5981fd1e744f0b2
DanieleChecchin <danielechecchin26@gmail.com>
2024-10-09 15:35:16 +0200

    classe + variabili d'istanza

---
+++ class/Movie.php
@@ -0,0 +1,14 @@
+<?php
+
+class Movie
+{
+    public $title;
+    public $genre;
+    public $year;
+}
+
+$avatar = new Movie();
+$avatar->title = 'Avatar';
+
+$minions = new Movie();
+$minions->title = 'Minions';
---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP OOP 1</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+    <!--create un file index.php in cui:
+è definita una classe ‘Movie’
+=> all'interno della classe sono dichiarate delle variabili d'istanza
+=> all'interno della classe è definito un costruttore
+=> all'interno della classe è definito almeno un metodo
+vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà
+-->
+    <main class="container py-5">
+
+        <h1 class="fw-bold text-center"> PHP OOP 1 </h1>
+
+    </main>
+</body>
+
+</html>
